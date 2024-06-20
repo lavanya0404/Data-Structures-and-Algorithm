393ccbad493bc84837e3bb9c6f0856da70613650
Lavanya Y M <lavanyamaruthi@0413.gmail.com>
2024-06-20 13:54:06 +0530

    update charArray

--- charArray.java
+++ charArray.java
@@ -1,5 +1,18 @@
+
+import java.util.Scanner;
+
 public class charArray {
+
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
     System.out.println("Hello world");
+    String ch;
+    System.out.println("Enter name");
+    {
+      ch = sc.next();
+    }
+    for (int i = 0; i < ch.length(); i++) {
+      System.err.println(ch.charAt(i));
+    }
   }
 }
