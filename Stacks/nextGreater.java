e7fd70548971c990ecba7b22888a78e80f07eb63
Lavanya <lavanyamaruthi@0413.gmail.com>
2024-09-01 21:03:47 +0530

    next greater element

---
+++ Stacks/nextGreater.java
@@ -0,0 +1,30 @@
+
+import java.util.Stack;
+
+public class nextGreater {
+  public static void nextGreat(int[] arr, int[] nextGreat) {
+    Stack<Integer> s = new Stack<Integer>();
+    for (int i = arr.length - 1; i >= 0; i--) {
+      while(!s.isEmpty() && (arr[s.peek()]<=arr[i]))
+      {
+        s.pop();
+      }
+      if(s.isEmpty())
+      {
+        nextGreat[i] = -1;
+      }
+      else{
+        nextGreat[i]= arr[s.peek()];
+      }
+      s.push(i);
+    }
+  }
+  public static void main(String[] args) {
+      int arr[] = {3,2,12,18,66};
+      int nextGreater[] = new int[arr.length];
+      nextGreat(arr, nextGreater);
+      System.out.println("Next Greater Element for given array is");
+    for (int i : nextGreater)
+      System.out.print(i+" ");
+  }
+}
