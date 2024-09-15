e3202e2570263a028a3d20bfdb009f72addeabde
Lavanya <lavanyamaruthi@0413.gmail.com>
2024-09-15 18:18:51 +0530

    Recursive traversal - Pre Post In Order traversal Iteratice traversal - Level Order traversal

--- Tree/PreOrderTree.java
+++ Tree/PreOrderTree.java
@@ -1,5 +1,3 @@
-package Tree;
-
 public class PreOrderTree {
 
     static class Node {
---
+++ TreeTraversal.java
@@ -0,0 +1,112 @@
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+// Recursive tree traversal - Pre order , In order , Post order
+public class TreeTraversal {
+
+    static class Node {
+
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree {
+
+        static int idx = -1;
+
+        public static Node buildTree(int[] nodes) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+            return newNode;
+        }
+        public static void preOrder(Node root)
+        {
+          if(root == null)
+          {
+            return;
+          }
+          System.out.print(root.data+" ");
+          preOrder(root.left);
+          preOrder(root.right);
+        }
+        public static void inOrder(Node root)
+        {
+          if(root == null)
+          {
+            return;
+          }
+          inOrder(root.left);
+          System.out.print(root.data+" ");
+          inOrder(root.right);
+        }
+        public static void postOrder(Node root)
+        {
+          if(root == null)
+          {
+            return;
+          }
+          postOrder(root.left);
+          postOrder(root.right);
+          System.out.print(root.data+" ");
+        }
+        // Level Order Travesrsal -- Iterative Approach
+        public static void levelOrder(Node root)
+        {
+          Queue<Node> q = new LinkedList<>();
+          if(root == null)
+          return;
+          q.add(root);
+          q.add(null);
+          while(!q.isEmpty())
+          {
+            Node curNode = q.remove();
+            if(curNode == null){
+            System.out.println();
+            if(q.isEmpty()){
+            break;
+            } else {
+              q.add(null);
+            }
+          }else{
+            System.out.print(curNode.data+" ");
+            if(curNode.left!=null)
+            {
+              q.add(curNode.left);
+            }
+            if(curNode.right!=null)
+            {
+              q.add(curNode.right);
+            }
+          }
+          }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        // System.out.println(root.data);
+        System.out.println("\nPre order");
+        tree.preOrder(root);
+      System.out.println("\nIn order");
+        tree.inOrder(root);
+        System.out.println("\nPost Order");
+        tree.postOrder(root);
+        System.out.println("\nLevel Order");
+        tree.levelOrder(root);
+    }
+}
