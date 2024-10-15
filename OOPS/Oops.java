31b795ff414ee63ec769b2368bb109651b8abe6e
Lavanya <lavanyamaruthi@0413.gmail.com>
2024-10-15 13:21:28 +0530

    Oops

---
+++ OOPS/Oops.java
@@ -0,0 +1,39 @@
+public class Oops {
+  public static void main(String[] args) {
+    Dog fudge = new Dog();
+    fudge.breath();
+    fudge.legs(4);
+    fudge.eat();
+    fudge.breed();
+  }
+}
+class Animal {
+ String color;
+ void eat() {
+  System.out.println("Eats");}
+ void breath(){
+  System.out.println("Breathes");
+ }
+}
+class Mammal extends Animal {
+  void legs(int legs) {
+    System.out.println("I have "+legs+" legs");
+  }
+}
+class Dog extends Mammal {
+  void breed() {
+    System.out.println("I am a pug");
+  }
+}
+
+
+
+
+
+class Fish extends Animal
+{
+  void fins(int fin)
+  {
+    System.out.println("I have "+fin+" fins");
+  }
+}
