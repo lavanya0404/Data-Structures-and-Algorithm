569f38359e317033696b2b35a181f05478b5d7e8
Lavanya <lavanyamaruthi@0413.gmail.com>
2024-10-21 21:28:24 +0530

    Balanced BST And Largest Size of Balanced BST

--- Binary Search Tree/CreateBalancedBST.java
+++ Binary Search Tree/CreateBalancedBST.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 class CreateBalancedBST {
     static class Node {
         int data;
@@ -28,26 +30,36 @@ class CreateBalancedBST {
       }
       return root;
   }
-    public static Node createBST(int arr[], int st, int end)
+    public static Node createBST(ArrayList<Integer> arr, int st, int end)
     {
       if(st>end){
         return null;
       }
       int mid = (st+end)/2;
-      Node ele = new Node(arr[mid]);
+      Node ele = new Node(arr.get(mid));
       ele.left = createBST(arr, st, mid-1);
       ele.right = createBST(arr, mid+1, end);
       return ele;
     }
-
+    public static void getInorder(Node root,ArrayList<Integer> arr)
+    {
+        if(root == null){
+            return;
+        }
+        getInorder(root.left,arr);
+        // System.out.println(root.data);
+        arr.add(root.data);
+        getInorder(root.right, arr);
+    }
     public static void main(String args[]) {
         int arr[] = {3, 5, 6, 8, 10, 11};
         Node root = null;
         for(int i=0;i<arr.length;i++){
           root = insert(root, arr[i]);
         }
-        System.out.println(heightOfTree(root)+" "+ root.data);
-        Node secRoot = createBST(arr, 0, arr.length-1);
+        ArrayList<Integer> array2 = new ArrayList<>();
+        getInorder(root, array2);
+        Node secRoot = createBST(array2, 0, array2.size()-1);
         System.out.println(heightOfTree(secRoot)+" "+ secRoot.data);
     }
 }
